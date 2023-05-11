c5d90617cdcb588ed279fbbf3c0aa73f5f91a549
tonystark <chandravicky288@gmail.com>
2023-05-11 10:40:08 +0530

    first

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+
+const Footer=()=>{
+    <footer className="footer">
+        <h4>All right reserved</h4>
+        <div>
+        <a href="/#">Partners</a> 
+        <a href="/#">Help</a>
+        <a href="/#">Contact</a>
+        </div>
+
+    </footer>
+}
+
+export default Footer;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+
+const Navbar = () => (
+    <nav className="navbar">
+          <h1>The Dojo Blog</h1>
+          <div className="links"> 
+                  <a href="/#">Home</a> 
+                  <a href="/#">Blog</a>
+                  <a href="/#">Contact</a>
+                  <a href="/#">Projects</a>
+          </div>
+    </nav>
+)
+
+export default Navbar;
